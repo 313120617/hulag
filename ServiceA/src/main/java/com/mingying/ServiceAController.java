ab31343c574afb7ed3fd08aadbd0eff28881a661
hll <313120617@qq.com>
2019-09-22 21:28:58 +0800

    first commit

---
+++ ServiceA/src/main/java/com/mingying/ServiceAController.java
@@ -0,0 +1,15 @@
+package com.mingying;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ServiceAController {
+    @RequestMapping(value = "/sayHello/{name}",method = RequestMethod.GET)
+    public String sayHello(@PathVariable("name") String name){
+        System.out.println("serviceA");
+        return "{'msg':'hello," + name + "'}";
+    }
+}
